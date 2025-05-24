1be9681b7550bcbb9c0743416f1efd336072abcd
Tiajh <tiahenim25@gmail.com>
2025-05-25 03:30:57 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Halaman Utama
 Route::get('/', function () {
-    return view('welcome');
+    return view('beranda');
+})->name('beranda');
+
+//Halaman Jadwal Donor
+Route::get('/jadwal-donor', function () {
+    return view('jadwal');
+})->name('jadwal');
+
+// Halaman Info Donor
+Route::get('/info-donor', function () {
+    return view('info');
+})->name('info');
+
+//Halaman Tentang Kami
+Route::get('/tentang', function () {
+    return view('tentang');
 });
+
+// Halaman Pendaftaran
+Route::get('/daftar', function () {
+    return view('daftar');
+});
+// Menyimpan Data Pendaftar (contoh)
+Route::post('/daftar', [PendonorController::class, 'store'])->name('pendonor.store');
